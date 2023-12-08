39b54969906373a6d27d0c01edcb24e207d3df57
desmond235 <adabedesmond@gmail.com>
2023-12-08 12:06:55 -0800

    modified file

--- lib/main.dart
+++ lib/main.dart
@@ -18,16 +18,12 @@ void main() {
 class App extends StatelessWidget {
   const App({super.key});
 
--
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
       theme: theme,
       home: const CategoriesScreen(),
       debugShowCheckedModeBanner: false,
-
-
     );
   }
 }
