c558c738f1388f2675d9a433d5d756b1c9e145ea
desmond235 <adabedesmond@gmail.com>
2023-12-24 21:49:23 -0800

    added a navigation_bar_color file

---
+++ lib/navigation_bar_color.dart
@@ -0,0 +1,7 @@
+
+import 'package:flutter/services.dart';
+
+ SystemUiOverlayStyle overlayStyle = const SystemUiOverlayStyle(
+  systemNavigationBarColor: Color.fromARGB(229, 0, 0, 0),
+  systemNavigationBarIconBrightness: Brightness.light,
+);
